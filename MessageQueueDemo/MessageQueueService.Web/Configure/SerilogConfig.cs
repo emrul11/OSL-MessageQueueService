8b3f4fda661f7abaa1aa1e40cc66d9b707453ea7
emrul11 <emrulkabir111@gmail.com>
2024-01-18 11:42:13 +0600

    Serilog Configured in Web project

---
+++ MessageQueueDemo/MessageQueueService.Web/Configure/SerilogConfig.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessageQueueService.Web.Configure
+{
+    public class SerilogConfig
+    {
+        public static void Configure()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()                
+                .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+    }
+}
