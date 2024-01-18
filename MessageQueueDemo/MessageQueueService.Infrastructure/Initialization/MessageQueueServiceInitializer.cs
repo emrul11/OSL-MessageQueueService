34598e77718b84f2c57c6eb4e78c0624f35a9057
emrul11 <emrulkabir111@gmail.com>
2024-01-18 11:24:44 +0600

    Clean Setup with new structure

---
+++ MessageQueueDemo/MessageQueueService.Infrastructure/Initialization/MessageQueueServiceInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessageQueueService.Infrastructure.Initialization
+{
+    public class MessageQueueServiceInitializer
+    {
+    }
+}
---
+++ MessageQueueDemo/MessageQueueService.Publisher/Services/PublisherService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessageQueueService.Publisher.Services
+{
+    public class PublisherService
+    {
+    }
+}
